f7b935ba6fccb3dae67ba8951893bfbf523b3e71
ithsjeff <iths003.hsgroup@gmail.com>
2024-11-15 19:23:43 +0700

    fix: update build paths for Docker compatibility

--- remix.config.js
+++ remix.config.js
@@ -6,12 +6,11 @@ export default {
   appDirectory: "src/app",
   assetsBuildDirectory: "public/build",
   publicPath: "/build/",
-  serverBuildPath: "build/server/index.js",
+  serverBuildPath: "build/index.js",
   future: {
     v3_fetcherPersist: true,
     v3_lazyRouteDiscovery: true,
     v3_relativeSplatPath: true,
     v3_throwAbortReason: true,
-    v3_singleFetch: true
   },
 }
