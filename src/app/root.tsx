13150b7b033bdee4de8c9aafb47253c065ec1e35
arcestia <jeff@skiddle.id>
2025-01-02 16:55:53 +0700

    add: verif for adsense

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -46,6 +46,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-adsense-account" content="ca-pub-5163720941664319" />
         <Meta />
         <Links />
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
